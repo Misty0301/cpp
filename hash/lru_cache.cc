0c1123ffe2d4573594c1fe7e63c7dfe332d6088d
misty <misty03011@gmail.com>
2025-05-21 18:41:35 +0800

    change lru cache to template

--- hash/lru_cache.cc
+++ hash/lru_cache.cc
@@ -2,25 +2,14 @@
 #include <list>
 #include <unordered_map>
 
+template<typename K, typename V>
 class LRUCache {
+    public:
+        explicit LRUCache(int capacity):capacity_(capacity) {}
+
     private:
         int capacity_;
-        std::list<std::pair<int,int>> cache_;
-        std::unordered_map<int, std::list<std::pair<int,int>>::iterator> cache_map_;
-    public:
-        explicit LRUCache(int capacity): capacity_(capacity) {}
-
-        int get(int key){
-            if(cache_map_.find(key)==cache_map_.end()){
-                return -1;
-            }
-            cache_.splice(cache_.begin(),cache_,cache_map_[key]);
-        }
+        std::list<std::pair<K, V>> cache_;
+        std::unordered_map<K, typename std::list<std::pair<K, V>>::iterator> cache_map_; //typename用于告诉编译器iterator是个类型
         
-        void put(int key, int value){
-            auto it = cache_map_.find(key);
-            if(it!=cache_map_.end()){
-                cache_.splice(cache_.begin(),cache_,it->second);
-            }
-        }
 }
